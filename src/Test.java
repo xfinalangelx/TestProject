7043b15ee22afc03cbc2ef6bedd257bc3fda1af4
finalangel <55591218+xfinalangelx@users.noreply.github.com>
2021-01-20 23:01:56 +0800

    Added a line

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is a test project.");
+        System.out.println("This is the second line.");
     }
 }
